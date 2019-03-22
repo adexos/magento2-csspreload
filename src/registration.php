ad3b2d3ed7754c19f748b58baac4a5eec3df8af7
Antoine Daudenthun <adaudenthun@gmail.com>
2019-03-22 10:58:57 +0100

    Update registration.php

--- src/registration.php
+++ src/registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Meanbee_CSSPreload',
+    'Adexos_CSSPreload',
     __DIR__
 );
